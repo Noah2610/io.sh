6d76ff36cb8b76921fc75b01e64dd85fecdf4234
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2017-11-24 03:43:57 +0100

    Argument reading and functions working

--- io.sh
+++ io.sh
@@ -7,22 +7,45 @@
 ###
 ########################################
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd )"
+USAGE=$( cat "${DIR}/doc" )
+
+HELP="h"          # -h
+HELP_EXT="help"   # --help
+
+### Your defined arguments; Arguments that can be used:
+# WIP TODO
+OPTS=()
+OPTS_EXT=()
+ALL_OPTS=()
+KEYWORDS=()
+
+### Following are arguments from command line:
+ARGS_OPTS=()           # ex: -v -a
+ARGS_OPTS_EXT=()       # ex: --verbose --all
+ARGS_ALL_OPTS=()       # $ARGS_OPTS and $ARGS_OPTS_EXT
+ARGS_KEYWORDS=()       # ex: status enable
+
+
 function getio {
 
 	for arg in $@; do
 		if [[ $arg =~ ^-[[:alnum:]]+$ ]]; then
 			for (( i=1; i < ${#arg}; i++ )); do
-				OPTS+=("${arg:$i:1}")
+				ARGS_OPTS+=("${arg:$i:1}")
 			done
 		elif [[ "$arg" =~ ^--[[:alnum:]][[:alnum:]]+$ ]]; then
-			OPTS_EXT+=("${arg:2}")
+			ARGS_OPTS_EXT+=("${arg:2}")
 		elif [[ "$arg" =~ ^[[:alnum:]]+$ ]]; then
-			KEYWORDS+=("$arg")
+			ARGS_KEYWORDS+=("$arg")
 		fi
 	done
 
+	### ARGS_ALL_OPTS
+	ARGS_ALL_OPTS=($( echo "${ARGS_OPTS} ${ARGS_OPTS_EXT}" ))
+
 	### Print usage / help_text and exit
-	if [[ " ${OPTS[@]} " =~ " ${HELP} " || " ${OPTS_EXT[@]} " =~ " ${HELP_EXT} " ]]; then
+	if [[ " ${ARGS_OPTS[@]} " =~ " ${HELP} " || " ${ARGS_OPTS_EXT[@]} " =~ " ${HELP_EXT} " ]]; then
 		echo "$USAGE"
 		exit
 	fi
@@ -30,40 +53,30 @@ function getio {
 }
 
 function has_x? {
-	### $1 -> to check against
-	### $@ -> check any (except first)
-	skip=false
-	for O in $@; do
-		if [ $skip == false ]; then
-			skip=true
-			continue
-		fi
-		if [[ " $1 " =~ " $O " ]]; then
+	IFS_DEF=$IFS
+	IFS=','
+	to_check=($1)
+	inputs=($2)
+	IFS=$IFS_DEF
+
+	for input in ${inputs[@]}; do
+		if [[ " ${to_check[@]} " =~ " $input " ]]; then
 			echo -n "true"
 			return
 		fi
 	done
 	echo -n "false"
+
 }
 
 function has_opt? {
-	echo -n $( has_x? "${OPTS[@]} ${OPTS_EXT[@]}" $@ )
+	echo -n $( has_x? "$( echo ${ARGS_OPTS[@]} ${ARGS_OPTS_EXT[@]} | tr ' ' ',' )" "$( echo $@ | tr ' ' ',' )" )
 }
 
 function has_keyword? {
-	echo -n $( has_x? "${KEYWORDS[@]}" $@ )
+	echo -n $( has_x? "$( echo ${ARGS_KEYWORDS[@]} | tr ' ' ',' )" "$( echo $@ | tr ' ' ',' )" )
 }
 
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd )"
-USAGE=$( cat "${DIR}/doc" )
-
-HELP="h"          # -h
-HELP_EXT="help"   # --help
-
-OPTS=()
-OPTS_EXT=()
-KEYWORDS=()
 
 ### Get options and keywords from command line, fill variables above
 getio $@
@@ -73,21 +86,19 @@ getio $@
 ## return "false" if any option  WAS   given
 # has_opt? a test
 
-echo "KWS: ${KEYWORDS[@]}"
-echo "OPTS: ${OPTS[@]}"
-echo "OPTS_EXT: ${OPTS_EXT[@]}"
+#echo "KWS: ${ARGS_KEYWORDS[@]}"
+#echo "ARGS_OPTS: ${ARGS_OPTS[@]}"
+#echo "ARGS_OPTS_EXT: ${ARGS_OPTS_EXT[@]}"
 
-if [[ "$( has_opt? verbose )" =~ "true" ]]; then
-	echo "OPT"
-fi
-
-has_keyword? "status"
-exit
-if [[ "$( has_keyword? status )" =~ "true" ]]; then
-	echo "KEYWORD"
-else
-	echo "NO KW"
-fi
+#if [[ "$( has_opt? verbose )" =~ "true" ]]; then
+#	echo "OPT"
+#fi
+#
+#if [[ "$( has_keyword? status )" =~ "true" ]]; then
+#	echo "KEYWORD"
+#else
+#	echo "NO KW"
+#fi
 
 #case $( has_opt ) in
 
